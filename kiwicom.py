53da19fa8430fd2b4bf1361f85d71914a8b8b997
zfarkas88 <kufeee@gmail.com>
2016-09-13 21:20:25 +0000

    while response ok

--- kiwicom.py
+++ kiwicom.py
@@ -1,18 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-quote_page = ('https://www.kiwi.com/us/search/budapest-bud/-/anytime/2-10?passengers=2&priceMax=150&priceMin=0&sortBy=price')
-page = requests.get(quote_page)
-soup = BeautifulSoup(page.text,"lxml")
 
-file = open("out.log", "w")
-file.write(page.text)
-file.close()
+counter=0
 
-cities = soup.find_all('span', attrs={'class': 'city'})
-prices = soup.find_all('span', attrs={'class': 'price-content'})
+while counter<10:
+	quote_page = ('https://www.kiwi.com/us/search/budapest-bud/-/anytime/2-10?passengers=2&priceMax=50&priceMin=0&sortBy=price')
+	page = requests.get(quote_page)
+	#print ("Status code: ",page.status_code)
+	soup = BeautifulSoup(page.text,"lxml")
+
+	#file = open("out.log", "w")
+	#file.write(page.text)
+	#file.close()
+
+	cities = soup.find_all('span', attrs={'class': 'city'})
+	prices = soup.find_all('span', attrs={'class': 'price-content'})
+	if len(cities)>0:
+		break	
+	else:
+		counter=+1
+		time.sleep(5)	
 if len(cities)>0:
 	for i in range(1, len(cities)):
-		print (cities[i].string.strip(), " ", prices[i].findNext('span').findNext('span').findNext('span').string.strip())
+                        print (cities[i].string.strip(), " ", prices[i].findNext('span').findNext('span').findNext('span').string.strip())
 else:
-	print('Sikertelen! :(')
+	print("Sikertelen!")
