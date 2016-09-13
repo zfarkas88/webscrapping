671dda7cc59e8822a9b0c2ae812bd61dde2f7e8d
zfarkas88 <kufeee@gmail.com>
2016-09-13 17:29:34 +0000

    ok

--- kiwicom.py
+++ kiwicom.py
@@ -3,9 +3,12 @@ import requests
 import time
 quote_page = ('https://www.kiwi.com/us/search/budapest-bud/-/anytime/2-10?passengers=2&priceMax=150&priceMin=0&sortBy=price')
 page = requests.get(quote_page)
-print(page)
 soup = BeautifulSoup(page.text,"lxml")
-print(soup.prettify()[0:10])
+
+file = open("out.log", "w")
+file.write(page.text)
+file.close()
+
 cities = soup.find_all('span', attrs={'class': 'city'})
 prices = soup.find_all('span', attrs={'class': 'price-content'})
 if len(cities)>0:
